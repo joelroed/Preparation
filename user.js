4ee0e2c664d5c39770d7c91814281faab88c1299
Joel Roed <jooel.roed@gmail.com>
2022-03-25 20:27:11 +0100

    Changes

---
+++ user.js
@@ -0,0 +1,7 @@
+async function main() {
+    const id = localStorage.getItem("id");
+    const posts = await fetch (`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
+    const postsData = await posts.json();
+}
+
+main();
