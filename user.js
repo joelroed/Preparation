2ebe44dd1622d5e6e385cb9a245af66ccf5e65dd
Joel Roed <jooel.roed@gmail.com>
2022-03-27 15:05:23 +0200

    Finish

--- user.js
+++ user.js
@@ -1,10 +1,20 @@
 const postListEl = document.querySelector('.post-list')
+const id = localStorage.getItem("id");
 
 async function onSearchChange(event) {
     const id = event.target.value;
+    renderPosts(id)
+}
+
+
+async function renderPosts(id) {
     const posts = await fetch (`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
     const postsData = await posts.json();
-    postListEl.innerHTML = postsData.map(post => `
+    postListEl.innerHTML = postsData.map(post => postHTML(post)).join('');
+}
+
+function postHTML(post) {
+    return `
     <div class="post">
         <div class="post__title">
             ${post.title}
@@ -13,25 +23,7 @@ async function onSearchChange(event) {
             ${post.body}
         </p>
     </div>
-    `).join('');
+    `
 }
 
-
-async function main() {
-    const id = localStorage.getItem("id");
-    const posts = await fetch (`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
-    const postsData = await posts.json();
-
-    postListEl.innerHTML = postsData.map(post => `
-    <div class="post">
-        <div class="post__title">
-            ${post.title}
-        </div>
-        <p class="post__body">
-            ${post.body}
-        </p>
-    </div>
-    `).join('');
-}
-
-main();
+renderPosts(id);
